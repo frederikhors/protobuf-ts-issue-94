06a7d7fc5f799c6f54e0b8f67ca0af323d6e9c78
frederikhors <41120635+frederikhors@users.noreply.github.com>
2021-04-06 12:53:30 +0200

    add protobuf-ts

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -2,4 +2,6 @@
 	import '../app.css';
 </script>
 
+<a href="/">Home</a> | <a href="protobuf">Protobuf</a>
+
 <slot />
---
+++ src/routes/protobuf/index.svelte
@@ -0,0 +1,6 @@
+<script>
+	import { ScalarValuesMessage } from './msg-scalar';
+	console.log(ScalarValuesMessage.create());
+</script>
+
+<h1>Protobuf!</h1>
